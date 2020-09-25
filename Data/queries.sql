6de268020c932c60ffe4efc075b8bb6b76b055cc
Brij Raj <brij.raj.singh2710@gmail.com>
2020-09-25 16:31:22 +0100

    select date range & insert data only if not present in db

--- Data/queries.sql
+++
@@ -1,4 +0,0 @@
-SELECT count(id) as db_record, ( SELECT DATEDIFF("2020-09-15", "2020-09-01")) as num_of_days FROM `covid_stats` WHERE country_code='IN' AND last_updated BETWEEN ('2020-09-01') AND ('2020-09-15') ORDER BY `covid_stats`.`last_updated` ASC 
-
-
-SELECT count(id) as db_record, ( SELECT DATEDIFF('2020-09-24', '2020-09-01')) as num_of_days FROM `covid_stats` WHERE country_code='IN' AND last_updated BETWEEN ('2020-09-01') AND ('2020-09-24') ORDER BY `covid_stats`.`last_updated` ASC;
