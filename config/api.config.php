6d582c8c65087d3eec41f65fc2f9b875c63d2ac2
Brij Raj <brij.phpdev@gmail.com>
2020-09-25 14:01:05 +0100

    initial commit

--- config/api.config.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$api_url =  'http://api.coronatracker.com/v3/analytics/newcases/country';
--- config/db.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db = array();
-
-$db['host'] = 'localhost';
-$db['database'] = 'covid';
-$db['user'] = 'root';
-$db['password'] = '';
